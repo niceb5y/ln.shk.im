8e8152aa5f4244d6e0f91ea7bf92d8fd67547846
Seungho Kim <niceb5y@gmail.com>
2019-08-06 01:34:01 +0900

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# ln.shk.im
+
+Serverless URL shortner
+
+Powered by Serverless Framework & AWS DynamoDB
---
+++ handler.ts
@@ -0,0 +1,47 @@
+import { APIGatewayProxyHandler } from 'aws-lambda'
+import * as AWS from 'aws-sdk/global'
+import * as DynamoDB from 'aws-sdk/clients/dynamodb'
+
+export const index: APIGatewayProxyHandler = async () => {
+  return {
+    statusCode: 301,
+    headers: {
+      Location: process.env.PAGE_INDEX
+    },
+    body: ''
+  }
+}
+
+export const link: APIGatewayProxyHandler = async event => {
+  AWS.config.update({
+    region: 'ap-northeast-2'
+  })
+
+  const docClient = new DynamoDB.DocumentClient()
+
+  var params = {
+    TableName: process.env.DB_TABLE_NAME,
+    Key: {
+      id: event.pathParameters['link_id']
+    }
+  }
+
+  try {
+    const result = await docClient.get(params).promise()
+    return {
+      statusCode: 301,
+      headers: {
+        Location: result.Item['location']
+      },
+      body: ''
+    }
+  } catch (err) {
+    return {
+      statusCode: 301,
+      headers: {
+        Location: process.env.PAGE_404
+      },
+      body: ''
+    }
+  }
+}
